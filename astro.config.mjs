703c093b6ea94b2b1d12c58e3c4a7dd83b1fe715
Neosergio09 <se.kamilo28@gmail.com>
2025-12-22 02:44:07 -0500

    Estructura completa: Páginas, estilos y layouts de Otterock

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ¡ESTA LÍNEA ES LA QUE QUITA EL ERROR 404!
+  site: 'https://otterock-web.vercel.app', 
+  
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [sitemap()]
+});
